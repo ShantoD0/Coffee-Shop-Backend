8574c71716ca2b6e2eef8ab23bc8fba449dd9212
shanto <shanto22e@gmail.com>
2025-05-19 11:54:42 +0600

    added get req

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express=require('express')
 const cors=require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const students = require('./public/student.json');
+const dotenv = require('dotenv').config();
 
 
 const app=express()
 app.use(cors())
 app.use(express.json())
 const port=2000
+const name=process.env.NAME
+const pass=process.env.PASS
+
 
 app.get('/',(req,res) => {
 res.send('Hello World')
 })
 
 
-const uri = "mongodb+srv://shanto:cBNV5fVcTybbMiFU@cluster0.f1kjav4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${name}:${pass}@cluster0.f1kjav4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,15 +34,20 @@ async function run() {
     // Send a ping to confirm a successful connection
     const collection=client.db('coffee_shop').collection('coffee')
 
-    // app.get('/coffees',(req,res) => {
-      
-    // })
+    app.post('/coffees',async (req,res) => {
+      const result=await collection.insertOne(req.body)
+      res.send(result)
+    })
+    app.get('/coffees',async (req,res) => {
+      const result=await collection.find().toArray()
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
